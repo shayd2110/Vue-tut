d5fab10345ca87854302266401c266b7e076095f
shayd2110 <shayd2110@gmail.com>
2025-07-16 12:12:59 +0300

    Squashed 'modal-project/' content from commit 42dc598
    git-subtree-dir: modal-project
    git-subtree-split: 42dc598c0970f73ef436a334ccdb8d8bc533fdce

---
+++ src/components/Model.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <div class="modal-header">
+        <!-- Uncomment to use props -->
+        <!-- <h1>{{ header }}</h1>
+        <p>{{ text }}</p> -->
+        <slot>default content </slot>
+        <div class="actions">
+          <slot name="links"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // Define props to accept data from parent component
+  props: ["theme"],
+  // Uncomment to use props
+  // props: ["header", "text", "theme"],
+  methods: {
+    // You can add methods here if needed
+    closeModal() {
+      // Logic to close the modal can be added here
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+.backdrop {
+  top: 0;
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background-color: white;
+  border-radius: 10px;
+}
+.modal h1 {
+  color: #03cfb4;
+  border: none;
+  padding: 0%;
+}
+.modal p {
+  font-style: normal;
+}
+.modal .actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+}
+.modal .actions a {
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 5px;
+  text-decoration: none;
+  margin: 10px;
+}
+.modal.sale {
+  background-color: crimson;
+  color: white;
+}
+.modal.sale h1 {
+  color: white;
+}
+.modal.sale .actions a {
+  color: white;
+  border: 1px solid #fff;
+}
+</style>
