66afbfa600b5fc5e93e48e9be7d8dadb1746bbf8
shayd2110 <shayd2110@gmail.com>
2025-07-16 14:12:30 +0300

    feat: starting the game

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,11 +1,31 @@
 <template>
   <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
+import Block from "./components/Block.vue";
 export default {
   name: "App",
-  components: {},
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      // Logic to start the reaction timer game
+      console.log("Game started!");
+      // Set a random delay between 2 and 7 seconds
+      this.delay = Math.floor(Math.random() * 5000) + 2000;
+      // Simulate a delay before the game starts
+      this.isPlaying = true;
+      console.log(`Wait for ${this.delay} milliseconds...`);
+    },
+  },
 };
 </script>
 
