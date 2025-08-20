5dcdf7e20e4a6111491813ff5eeac0b8a8e93468
shayd2110 <shayd2110@gmail.com>
2025-08-20 09:32:15 +0300

    Make it work

--- web-form/.eslintrc.js
+++ web-form/.eslintrc.js
@@ -3,16 +3,17 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  parser: "vue-eslint-parser",
   parserOptions: {
     parser: "@babel/eslint-parser",
+    ecmaVersion: 2020,
+    sourceType: "module",
+    requireConfigFile: false,
   },
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/no-multiple-template-root": "off",
   },
 };
