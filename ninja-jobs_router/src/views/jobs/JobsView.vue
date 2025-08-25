447edc9f5f43bac499a5707db363377bc9b7a9dd
shayd2110 <shayd2110@gmail.com>
2025-08-25 10:35:15 +0300

    Add class to job items and comment out company display in JobsView

--- ninja-jobs_router/src/views/jobs/JobsView.vue
+++ ninja-jobs_router/src/views/jobs/JobsView.vue
@@ -1,10 +1,10 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id">
+  <div v-for="job in jobs" :key="job.id" class="job">
     <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
       <h2>{{ job.title }}</h2>
     </router-link>
-    <p>Company: {{ job.company }}</p>
+    <!-- <p>Company: {{ job.company }}</p> -->
   </div>
 </template>
 
