741e824408ab064cb806ab86a2295c9b3a007ad6
shayd2110 <shayd2110@gmail.com>
2025-08-25 11:11:27 +0300

    Add  Programmatic navigation

--- ninja-jobs_router/src/App.vue
+++ ninja-jobs_router/src/App.vue
@@ -3,8 +3,27 @@
     <router-link to="/">Home</router-link> | <router-link :to="{ name: 'about' }">About</router-link> |
     <router-link :to="{ name: 'jobs' }">Jobs</router-link>
   </nav>
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
   <router-view />
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: "about" });
+    },
+    back() {
+      this.$router.go(-1);
+    },
+    forward() {
+      this.$router.go(1);
+    },
+  },
+};
+</script>
 
 <style>
 #app {
@@ -30,5 +49,11 @@ nav a {
 nav a.router-link-exact-active {
   color: white;
   background-color: crimson;
+}
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>
