3968d8a4dea81065a6a95b0eedd3cd98d6c787c9
shayd2110 <shayd2110@gmail.com>
2025-08-25 13:14:24 +0300

    Using refs for reactivity

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,24 +1,38 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>{{ name }} is {{ age }} years old.</p>
+    <p ref="p">{{ name }} is {{ age }} years old.</p>
     <button @click="handleClick">Click me</button>
+    <button @click="age++">inc. age</button>
+    <input type="text" v-model="name" />
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "HomeView",
   setup: () => {
+    /* declare p here to use it in setup()     
     console.log("setup");
-    let name = "mort";
-    let age = 9;
+    console.log("this", this); // undefined
+
+    const p = ref("hello"); // works
+    console.log(p, p.value); // p is a ref object, p.value is the DOM element
+ */
+    const name = ref("mort");
+    const age = ref(9);
 
     const handleClick = () => {
-      console.log("you clicked me");
+      /*       
+      console.log(p, p.value);
+      p.value.classList.add("test");
+      p.value.textContent = "clicked!"; */
+      name.value = "morty";
     };
 
-    return { name, age, handleClick };
+    return { name, age, handleClick /* , p */ };
   },
 };
 </script>
