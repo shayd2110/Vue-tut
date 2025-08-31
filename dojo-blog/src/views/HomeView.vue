abd8a84924def0118f3cade0d086042d996ad89d
shayd2110 <shayd2110@gmail.com>
2025-08-31 13:19:57 +0300

    computed values

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -7,17 +7,23 @@
     <button @click="age++">inc. age</button>
     <input type="text" v-model="name" />
      -->
-    <h2>Refs</h2>
+    <!--     <h2>Refs</h2>
     <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
     <button @click="updateNinjaOne">Update ninjaOne</button>
     <h2>Reactive</h2>
     <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo }}</p>
     <button @click="updateNinjaTwo">Update ninjaTwo</button>
+    -->
+    <input type="text" placeholder="search..." v-model="search" />
+    <p>search term: {{ search }}</p>
+    <!-- <div v-for="name in names" :key="name">{{ name }}</div> -->
+    <h3>Matched Names:</h3>
+    <div v-for="name in matchedNames" :key="name">{{ name }}</div>
   </div>
 </template>
 
 <script>
-import { reactive, ref } from "vue";
+import { computed, reactive, ref } from "vue";
 
 export default {
   name: "HomeView",
@@ -31,30 +37,44 @@ export default {
  */
     /*     const name = ref("mort");
     const age = ref(9); */
-    const ninjaOne = ref({ name: "mort", age: 9 });
+    /*     const ninjaOne = ref({ name: "mort", age: 9 });
     const ninjaTwo = reactive({ name: "netta", age: 0.1 });
 
     const nameOne = ref("mort");
     const nameTwo = reactive("netta");
+ */
 
-    const handleClick = () => {
-      /*       
-      console.log(p, p.value);
-      p.value.classList.add("test");
-      p.value.textContent = "clicked!"; */
-      name.value = "morty";
+    // computed values
+    const names = ref(["mort", "netta", "yossi", "dana", "itay", "dorit", "yoni"]);
+    const search = ref("");
+    const matchedNames = computed(() => {
+      return names.value.filter((name) => name.toLowerCase().includes(search.value.toLowerCase()));
+    });
+
+    // const handleClick = () => {
+    //   /*
+    //   console.log(p, p.value);
+    //   p.value.classList.add("test");
+    //   p.value.textContent = "clicked!"; */
+    //   name.value = "morty";
+    // };
+
+    // const updateNinjaOne = () => {
+    //   ninjaOne.value.age = 40;
+    // };
+
+    // const updateNinjaTwo = () => {
+    //   ninjaTwo.age = 1;
+    //   // nameTwo = "netta the great"; /* won't work, nameTwo is not an object */
+    // };
+
+    return {
+      /* name, age, handleClick  , p */
+      /* ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo, nameTwo */
+      names,
+      search,
+      matchedNames,
     };
-
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40;
-    };
-
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 1;
-      // nameTwo = "netta the great"; /* won't work, nameTwo is not an object */
-    };
-
-    return { /* name, age, handleClick  , p */ ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo, nameTwo };
   },
 };
 </script>
