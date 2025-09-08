fff0a2c103a5f3e15253cc898e69a95ad08b62f2
shayd2110 <shayd2110@gmail.com>
2025-09-08 12:14:31 +0300

    Make the functionality of order

--- ts/hyrule-jobs/src/components/JobsList.vue
+++ ts/hyrule-jobs/src/components/JobsList.vue
@@ -2,7 +2,7 @@
   <div class="job-list">
     <p>Ordered By {{ order }}</p>
     <ul>
-      <li v-for="job in jobs" :key="job.id">
+      <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
           <p>{{ job.salary }} rupees</p>
@@ -20,7 +20,7 @@
 <script lang="ts">
 import { Job } from "@/types/jobs";
 import { OrderTerm } from "@/types/OrderTerm";
-import { defineComponent, PropType } from "vue";
+import { computed, defineComponent, PropType } from "vue";
 
 export default defineComponent({
   props: {
@@ -32,6 +32,14 @@ export default defineComponent({
       required: true,
       type: String as PropType<OrderTerm>,
     },
+  },
+  setup(props) {
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1;
+      });
+    });
+    return { orderedJobs };
   },
 });
 </script>
