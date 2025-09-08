ad22ed270d58c524e2283536f2ac41550d5f2438
shayd2110 <shayd2110@gmail.com>
2025-09-08 14:52:25 +0300

    init stores

--- pinia/pinia-tasks/src/main.js
+++ pinia/pinia-tasks/src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/main.css";
+import { createPinia } from "pinia";
 
-createApp(App).mount('#app')
+createApp(App).use(createPinia).mount("#app");
