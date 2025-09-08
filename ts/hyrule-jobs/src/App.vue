2906f79348615ebea45921996da005cd09ba303a
shayd2110 <shayd2110@gmail.com>
2025-09-08 11:41:48 +0300

    init jobs

--- ts/hyrule-jobs/src/App.vue
+++ ts/hyrule-jobs/src/App.vue
@@ -1,29 +1,39 @@
 <template>
   <div class="app">
+    <div></div>
+    <!-- 
     hello {{ name }} - {{ age }}
-
     <label for="name">Name</label>
     <input id="name" type="text" v-model="newName" />
     <button @click="changeName(newName)">Change name</button>
 
     <label for="age">Age</label>
     <input id="age" type="text" v-model="newAge" />
-    <button @click="changeAge(newAge)">Change age</button>
+    <button @click="changeAge(newAge)">Change age</button> -->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref, toRefs } from "vue";
+import { Job } from "./types/jobs";
 
 export default defineComponent({
   name: "App",
   components: {},
   setup() {
-    const name = ref("Mort");
+    const jobs = ref<Job[]>([
+      { title: "farm worker", location: "lon lon ranch", salary: 30000, id: "1" },
+      { title: "quarryman", location: "death mountain", salary: 40000, id: "2" },
+      { title: "flute player", location: "the lost woods", salary: 35000, id: "3" },
+      { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+      { title: "prison guard", location: "gerudo valley", salary: 32000, id: "5" },
+    ]);
+    return { jobs };
+    /*     const name = ref("Mort");
     const age = ref<number | string>(25);
     const newName = ref("");
     const newAge = ref<number | string>("");
-    return { name, age, newName, newAge };
+    return { name, age, newName, newAge }; */
     /*     const state = reactive({
       name: "Mort",
       newName: "",
@@ -41,14 +51,14 @@ export default defineComponent({
     };
   }, */
   methods: {
-    changeName(newName: string) {
+    /*     changeName(newName: string) {
       this.name = newName;
       return name;
     },
     changeAge(newAge: number | string) {
       this.age = newAge;
       return newAge;
-    },
+    }, */
   },
 });
 </script>
