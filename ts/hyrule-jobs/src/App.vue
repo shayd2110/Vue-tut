2209f9d8ec2f8e327e810f88b84d8542358cd794
shayd2110 <shayd2110@gmail.com>
2025-09-08 14:38:28 +0300

    init pinia-tasks

--- ts/hyrule-jobs/src/App.vue
+++ ts/hyrule-jobs/src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="app">
     <header>
-      <button @click="handleClick('title')">order by title</button>
-      <button @click="handleClick('salary')">order by salary</button>
-      <button @click="handleClick('location')">order by location</button>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="Site logo" />
+        <h1>HYRULE JOBS</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
     </header>
     <JobsList :jobs="jobs" :order="order" />
     <!-- 
@@ -91,5 +97,16 @@ button {
   border-radius: 4px;
   cursor: pointer;
   font-weight: bold;
+}
+header .title {
+  display: flex;
+  justify-content: center;
+}
+header img {
+  width: 60px;
+  margin-right: 20px;
+}
+header h1 {
+  font-size: 3em;
 }
 </style>
