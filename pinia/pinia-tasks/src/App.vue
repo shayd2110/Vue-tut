22f936c6c98cd29e389585c4bfa486a95d1e7a73
shayd2110 <shayd2110@gmail.com>
2025-09-09 10:18:32 +0300

    getters

--- pinia/pinia-tasks/src/App.vue
+++ pinia/pinia-tasks/src/App.vue
@@ -6,9 +6,23 @@
       <h1>Pinia Tasks</h1>
     </header>
 
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All Tasks</button>
+      <button @click="filter = 'favs'">favorite Tasks</button>
+    </nav>
+
     <!-- task list -->
-    <div class="task-list">
+    <div class="task-list" v-if="filter == 'all'">
+      <p>You have {{ taskStore.totalCnt }} left</p>
       <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+    <div class="task-list" v-if="filter == 'favs'">
+      <p>You have {{ taskStore.favCnt }} favorite tasks left</p>
+
+      <div v-for="task in taskStore.favs" :key="task">
         <TaskDetails :task="task" />
       </div>
     </div>
@@ -16,6 +30,7 @@
 </template>
 
 <script>
+import { ref } from "vue";
 import TaskDetails from "./components/TaskDetails.vue";
 import { useTaskStore } from "./stores/TaskStore";
 export default {
@@ -23,7 +38,9 @@ export default {
   setup() {
     const taskStore = useTaskStore();
 
-    return { taskStore };
+    const filter = ref("all");
+
+    return { taskStore, filter };
   },
 };
 </script>
--- pinia/pinia-tasks/src/stores/TaskStore.js
+++ pinia/pinia-tasks/src/stores/TaskStore.js
@@ -6,6 +6,20 @@ export const useTaskStore = defineStore("taskStore", {
       { id: 1, title: "buy some milk", isFav: false },
       { id: 2, title: "play Gloomhaven", isFav: true },
     ],
-    // name: "Morti",
   }),
+  getters: {
+    favs() {
+      return this.tasks.filter((t) => t.isFav);
+    },
+
+    favCnt() {
+      return this.tasks.reduce((prev, curr) => {
+        return curr.isFav ? prev + 1 : prev;
+      }, 0);
+    },
+
+    totalCnt: (state) => {
+      return state.tasks.length;
+    },
+  },
 });
